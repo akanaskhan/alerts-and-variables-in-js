19dfb8efb7786d236ca72dc38751ce65de131ac1
akanaskhan <muhammadanaskhanak@gmail.com>
2024-02-26 23:14:52 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,95 @@
+//Chapter no. 01
+
+//Q1
+// alert("Error! Please enter a valid password")
+
+//Q2
+// alert("Welcome to JS land... \nHappy Coding!")
+
+//Q3
+// alert("Welcome to JS land")
+// alert("Happy Coding")
+
+//Q4
+// alert("Welcome to JS land")
+// alert("Happy Coding")
+
+//Q5
+// console.log("Hello... I can run JS through my web browser's console")
+
+//Q6
+//its done
+
+
+//Chapter no.2
+
+//Q1
+// var username = "akanaskhan"
+// alert(username)
+
+//Q2
+// var myname = "MUHAMMAD ANAS"
+// alert(myname)
+
+//Q3
+// var message = "Hello World"
+// alert(message)
+
+//Q4
+// var StudentName = "Muhammad Anas"
+// alert(StudentName)
+// var age = "20 years old"
+// alert(age)
+// var certification = "Certified Web and Mobile Development"
+// alert(certification)
+
+//Q5
+// var food = "PIZZA\nPIZZ\nPIZ\nPI\nP\n"
+// alert(food)
+
+//Q6
+// var email = "muhammadanaskhanak@gmail.com"
+// alert("My email address is " + email)
+
+//Q7
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the book " + book)
+
+//Q8
+//written in HTML
+
+//Q9
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(design)
+
+//Chapter no.3
+
+//Q1
+// var age = "I am 20 years old"
+// alert(age)
+
+//Q2
+// var VisitCount = "You have visited this site 14 times"
+// alert(VisitCount)
+
+//Q3
+//written in HTML
+
+//Q4
+
+// var visitorName = "John Doe";
+// var productTitle = "T-Shirt's";
+// var quantity = 5;
+
+// document.write(
+//   "<b>" +
+//     visitorName +
+//     "</b>" +
+//     " ordered " +
+//     "<b>" +
+//     quantity +
+//     " " +
+//     productTitle +
+//     "</b>" +
+//     " on XYZ Clothing Store"
+// );
